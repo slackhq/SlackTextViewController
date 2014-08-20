f99d0e2927b432ae10414a596007464ab68f4b24
dzenbot <iromero@dzen.cl>
2014-08-20 19:41:56 -0400

    Auto-layout refactoring, fixing some breaking changes in iOS8

--- Source/SCKTextContainerView.m
+++ Source/SCKTextContainerView.m
@@ -15,6 +15,9 @@ NSString * const SCKInputAccessoryViewKeyboardFrameDidChangeNotification = @"com
 
 @interface SCKTextContainerView () <UITextViewDelegate>
 @property (nonatomic, copy) NSString *rightButtonTitle;
+
+@property (nonatomic, strong) NSLayoutConstraint *rightButtonWC;
+
 @end
 
 @implementation SCKTextContainerView
@@ -30,15 +33,6 @@ NSString * const SCKInputAccessoryViewKeyboardFrameDidChangeNotification = @"com
     return self;
 }
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        [self configure];
-    }
-    return self;
-}
-
 - (void)configure
 {
     self.translucent = NO;
@@ -48,20 +42,18 @@ NSString * const SCKInputAccessoryViewKeyboardFrameDidChangeNotification = @"com
     [self addSubview:self.rightButton];
     [self addSubview:self.textView];
     
-    [self updateConstraints];
+    [self setupViewConstraints];
     
-    // textView notifications
-//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didChangeTextView:) name:UITextViewTextDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didChangeTextView:) name:UITextViewTextDidChangeNotification object:nil];
 }
 
 - (void)dealloc
 {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UITextViewTextDidChangeNotification object:nil];
+    
     _leftButton = nil;
     _rightButton = nil;
     _textView = nil;
-    
-    // textView notifications
-//    [[NSNotificationCenter defaultCenter] removeObserver:self name:UITextViewTextDidChangeNotification object:nil];
 }
 
 
@@ -80,8 +72,10 @@ NSString * const SCKInputAccessoryViewKeyboardFrameDidChangeNotification = @"com
         _textView.translatesAutoresizingMaskIntoConstraints = NO;
         _textView.font = [UIFont systemFontOfSize:15.0f];
         _textView.maxNumberOfLines = 6;
-        _textView.autocorrectionType = UITextAutocorrectionTypeNo; // for debugging purpose
+        _textView.autocorrectionType = UITextAutocorrectionTypeNo; // For debug purpose
+        _textView.spellCheckingType = UITextSpellCheckingTypeNo; // For debug purpose
         _textView.keyboardType = UIKeyboardTypeTwitter;
+        _textView.returnKeyType = UIReturnKeyDefault;
         _textView.enablesReturnKeyAutomatically = YES;
         _textView.scrollIndicatorInsets = UIEdgeInsetsMake(0, -1, 0, 1);
         _textView.inputAccessoryView = [SCKInputAccessoryView new];
@@ -136,6 +130,19 @@ NSString * const SCKInputAccessoryViewKeyboardFrameDidChangeNotification = @"com
     return height;
 }
 
+- (CGFloat)rightButtonWidth
+{
+    NSString *title = [self.rightButton titleForState:UIControlStateNormal];
+    CGSize rigthButtonSize = [title sizeWithAttributes:@{NSFontAttributeName: self.rightButton.titleLabel.font}];
+    
+    if (self.autoHideRightButton) {
+        if (self.textView.text.length == 0) {
+            return 0.0;
+        }
+    }
+    return rigthButtonSize.width+kTextViewHorizontalPadding;
+}
+
 
 #pragma mark - Setters
 
@@ -144,19 +151,6 @@ NSString * const SCKInputAccessoryViewKeyboardFrameDidChangeNotification = @"com
     self.barTintColor = color;
     self.textView.inputAccessoryView.backgroundColor = color;
 }
-
-
-- (void)didMoveToSuperview
-{
-    [super didMoveToSuperview];
-    
-    // We save the right button title for later so when the textView is not first responder, we hide the button.
-    if (self.autoHideRightButton) {
-        self.rightButtonTitle = [self.rightButton titleForState:UIControlStateNormal];
-        [self.rightButton setTitle:@"" forState:UIControlStateNormal];
-    }
-}
-
 
 #pragma mark - UITextViewDelegate
 
@@ -168,8 +162,10 @@ NSString * const SCKInputAccessoryViewKeyboardFrameDidChangeNotification = @"com
     return YES;
 }
 
-- (void)textViewDidChange:(UITextView *)textView
+- (void)didChangeTextView:(NSNotification *)notification
 {
+    SCKTextView *textView = (SCKTextView *)notification.object;
+    
     // If it's not the expected textView, return.
     if (![textView isEqual:self.textView]) {
         return;
@@ -177,82 +173,74 @@ NSString * const SCKInputAccessoryViewKeyboardFrameDidChangeNotification = @"com
     
     BOOL enableRightButton = (self.textView.text.length > 0) ? YES : NO;
     
-    // If the button should automatically hide when the text view is empty
-    if (self.autoHideRightButton) {
-        
-        NSString *title = enableRightButton ? self.rightButtonTitle : @"";
-        NSString *rightTitle = [self.rightButton titleForState:UIControlStateNormal];
-        
-        // If titles don't match, update the title and the button constraints
-        if (![title isEqualToString:rightTitle]) {
-            [self.rightButton setTitle:title forState:UIControlStateNormal];
-            [self updateConstraintsAnimated:YES];
-        }
-    }
-    
     if (self.rightButton.enabled != enableRightButton) {
         [self.rightButton setEnabled:enableRightButton];
     }
+    
+    if (self.autoHideRightButton)
+    {
+        CGFloat rightButtonNewWidth = [self rightButtonWidth];
+        
+        if (self.rightButtonWC.constant == rightButtonNewWidth) {
+            return;
+        }
+        
+        self.rightButtonWC.constant = rightButtonNewWidth;
+        
+        if (rightButtonNewWidth > 0) {
+            [self.rightButton sizeToFit];
+        }
+        
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+             usingSpringWithDamping:0.7
+              initialSpringVelocity:0.7
+                            options:UIViewAnimationOptionBeginFromCurrentState|UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             [self layoutIfNeeded];
+                         }
+                         completion:NULL];
+    }
 }
-
-
-//#pragma mark - Notifications
-//
-//- (void)didChangeTextView:(NSNotification *)notification
-//{
-//    
-//}
 
 
 #pragma mark - View Auto-Layout
 
-- (void)updateConstraints
+- (void)setupViewConstraints
 {
     // Removes all constraints
     [self removeConstraints:self.constraints];
 
-    CGFloat leftButtonMargin = 0;
     UIImage *leftButtonImg = [self.leftButton imageForState:UIControlStateNormal];
-    if (leftButtonImg) {
-        leftButtonMargin = roundf((self.minHeight - leftButtonImg.size.height) / 2.0f);
-    }
     
-    CGFloat rightButtonMargin = roundf((self.minHeight - self.rightButton.frame.size.height) / 2.0f);
+    [self.rightButton sizeToFit];
     
-    NSString *rightTitle = [self.rightButton titleForState:UIControlStateNormal];
-    CGSize rigthButtonSize = [rightTitle sizeWithAttributes:@{NSFontAttributeName: self.rightButton.titleLabel.font}];
-    CGFloat rightButtonWidth = rigthButtonSize.width+kTextViewHorizontalPadding/2.0f;
+    CGFloat leftBottomMargin = roundf((self.minHeight - leftButtonImg.size.height) / 2.0f);
+    CGFloat rightBottomMargin = roundf((self.minHeight - CGRectGetHeight(self.rightButton.frame)) / 2.0f);
+
+    NSDictionary *views = @{@"textView": self.textView,
+                            @"leftButton": self.leftButton,
+                            @"rightButton": self.rightButton
+                            };
     
-    NSDictionary *views = @{@"textView": self.textView, @"leftButton": self.leftButton, @"rightButton": self.rightButton};
-    NSDictionary *metrics = @{@"hor" : @(kTextViewHorizontalPadding), @"ver" : @(kTextViewVerticalPadding), @"leftButtonMargin" : @(leftButtonMargin), @"rightButtonMargin" : @(rightButtonMargin), @"rightButtonWidth": @(rightButtonWidth)};
+    NSDictionary *metrics = @{@"hor" : @(kTextViewHorizontalPadding),
+                              @"ver" : @(kTextViewVerticalPadding),
+                              @"leftBottomMargin" : @(leftBottomMargin),
+                              @"rightBottomMargin" : @(roundf(rightBottomMargin)),
+                              };
     
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(==hor)-[leftButton]-(==hor)-[textView]-(==hor)-[rightButton(rightButtonWidth)]-(==hor)-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=leftButtonMargin)-[leftButton]-(==leftButtonMargin)-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=rightButtonMargin)-[rightButton]-(==rightButtonMargin)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(==hor)-[leftButton]-(==hor)-[textView]-(==hor)-[rightButton(==0)]-(==hor)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=leftBottomMargin)-[leftButton]-(==leftBottomMargin)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=rightBottomMargin)-[rightButton]-(==rightBottomMargin)-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(==ver)-[textView]-(==ver)-|" options:0 metrics:metrics views:views]];
     
-    [super updateConstraints];
-}
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"firstAttribute = %d", NSLayoutAttributeWidth];
+    NSArray *widthConstraints = [self.constraints filteredArrayUsingPredicate:predicate];
 
-- (void)updateConstraintsAnimated:(BOOL)animated
-{
-    [self updateConstraints];
+    self.rightButtonWC = widthConstraints[0];
+    self.rightButtonWC.constant = [self rightButtonWidth];
     
-    if (!animated) {
-        [self layoutIfNeeded];
-        return;
-    }
-    
-    [UIView animateWithDuration:0.3
-                          delay:0.0
-         usingSpringWithDamping:0.6
-          initialSpringVelocity:0.5
-                        options:UIViewAnimationOptionBeginFromCurrentState|UIViewAnimationOptionCurveEaseInOut
-                     animations:^{
-                         [self.rightButton sizeToFit];
-                         [self layoutIfNeeded];
-                     }
-                     completion:NULL];
+    [self layoutIfNeeded];
 }
 
 @end
