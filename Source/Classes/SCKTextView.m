cb3561ad53ed6435f6c537da31e567085b10ca8e
dzenbot <iromero@dzen.cl>
2014-09-22 18:55:42 -0300

    Updates the license

--- Source/Classes/SCKTextView.m
+++ Source/Classes/SCKTextView.m
@@ -1,11 +1,17 @@
 //
-//  SCKTextView.h
-//  SlackChatKit
-//  https://github.com/tinyspeck/slack-chat-kit
+//   Copyright 2014 Slack Technologies, Inc.
 //
-//  Created by Ignacio Romero Zurbuchen on 8/15/14.
-//  Copyright (c) 2014 Slack Technologies, Inc. All rights reserved.
-//  Licence: MIT-Licence
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 //
 
 #import "SCKTextView.h"
