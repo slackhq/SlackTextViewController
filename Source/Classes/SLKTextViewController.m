4207b1e7bab7aa3b04b6a4ad44cbf6df69e72d29
dzenbot <iromero@dzen.cl>
2014-09-28 16:20:11 -0300

    Includes asserts in initialisers methods

--- Source/Classes/SLKTextViewController.m
+++ Source/Classes/SLKTextViewController.m
@@ -72,6 +72,8 @@
 
 - (instancetype)initWithTableViewStyle:(UITableViewStyle)style
 {
+    NSAssert([self class] != [SLKTextViewController class], @"Oops! You must subclass SLKTextViewController.");
+
     if (self = [super initWithNibName:nil bundle:nil])
     {
         [self tableViewWithStyle:style];
@@ -82,6 +84,8 @@
 
 - (instancetype)initWithCollectionViewLayout:(UICollectionViewLayout *)layout
 {
+    NSAssert([self class] != [SLKTextViewController class], @"Oops! You must subclass SLKTextViewController.");
+
     if (self = [super initWithNibName:nil bundle:nil])
     {
         [self collectionViewWithLayout:layout];
@@ -90,8 +94,10 @@
     return self;
 }
 
-- (id)initWithCoder:(NSCoder *)decoder
+- (instancetype)initWithCoder:(NSCoder *)decoder
 {
+    NSAssert([self class] != [SLKTextViewController class], @"Oops! You must subclass SLKTextViewController.");
+    
     if (self = [super initWithCoder:decoder])
     {
         UITableViewStyle tableViewStyle = [[self class] tableViewStyleForCoder:decoder];
