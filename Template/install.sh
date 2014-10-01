8a73d52d21642cd9b77fa6493cf3cfeb5976b39c
dzenbot <iromero@dzen.cl>
2014-10-01 01:40:57 -0300

    Updates the installation script

--- Template/install.sh
+++ Template/install.sh
@@ -3,6 +3,9 @@
 # Default the folder name
 folderName="Slack Templates"
 
+# Determine the source directory.
+sourceDirectory=`dirname "$BASH_SOURCE"`
+
 # Determine the install directory.
 installDirectory=~/Library/Developer/Xcode/Templates/File\ Templates/"$folderName"
 
@@ -11,14 +14,11 @@ echo "Templates will be installed to $installDirectory"
 # Delete the install directory if it already exists to prevent deleted files from lingering.
 if [ -d "$installDirectory" ]
 then
-rm -r "$installDirectory"
+	rm -r "$installDirectory"
 fi
 
 # Create the install directory.
 mkdir -p "$installDirectory"
 
 # Copy all of the xctemplate folders into the install directory.
-cp -r *.xctemplate "$installDirectory"
-
-# Create empty directories that the project templates will copy.
-mkdir -p "$installDirectory"/"SlackTextView Controller.xctemplate/Supporting Files/"
+cp -r "$sourceDirectory"/*.xctemplate "$installDirectory"
