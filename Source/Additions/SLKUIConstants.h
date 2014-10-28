d8704a47c2c78a199a15b40a97f24133a32caa59
dzenbot <iromero@dzen.cl>
2014-10-28 18:15:01 -0300

    Disables keyboard notification debug (upsi)

--- Source/Additions/SLKUIConstants.h
+++ Source/Additions/SLKUIConstants.h
@@ -24,7 +24,7 @@
 #define UI_IS_IOS8_AND_HIGHER   ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0)
 
 #define SLK_INPUT_ACCESSORY_DEBUG 0             // Renders a translucent red area representing the keyboard accessory view
-#define SLK_KEYBOARD_NOTIFICATION_DEBUG 1       // Logs every keyboard notification being sent
+#define SLK_KEYBOARD_NOTIFICATION_DEBUG 0       // Logs every keyboard notification being sent
 
 typedef NS_ENUM(NSUInteger, SLKQuicktypeBarMode) {
     SLKQuicktypeBarModeHidden,
