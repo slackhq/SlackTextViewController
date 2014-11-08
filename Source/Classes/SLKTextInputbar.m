9fcf06ac6f7004e4aacb6536b375d1cb03f08289
dzenbot <iromero@dzen.cl>
2014-11-08 02:01:41 -0300

    Fixes blurry rendered text on non-retina devices (ie: iPad2)

--- Source/Classes/SLKTextInputbar.m
+++ Source/Classes/SLKTextInputbar.m
@@ -119,7 +119,7 @@
         _textView.returnKeyType = UIReturnKeyDefault;
         _textView.enablesReturnKeyAutomatically = YES;
         _textView.scrollIndicatorInsets = UIEdgeInsetsMake(0, -1, 0, 1);
-        _textView.textContainerInset = UIEdgeInsetsMake(8.0, 3.5, 8.0, 0.0);
+        _textView.textContainerInset = UIEdgeInsetsMake(8.0, 4.0, 8.0, 0.0);
         _textView.layer.cornerRadius = 5.0;
         _textView.layer.borderWidth = 0.5;
         _textView.layer.borderColor =  [UIColor colorWithRed:200.0/255.0 green:200.0/255.0 blue:205.0/255.0 alpha:1.0].CGColor;
