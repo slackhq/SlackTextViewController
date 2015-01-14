493be0a83a4e9af65fa878f8e188e4e0311b3173
dzenbot <iromero@dzen.cl>
2015-01-13 22:14:19 -0300

    Fixes issue regarding left/right arrow keys from not being recognized while using an external keyboard

--- Source/Classes/SLKTextViewController.m
+++ Source/Classes/SLKTextViewController.m
@@ -1759,8 +1759,6 @@ NSString * const SLKKeyboardDidHideNotification =   @"SLKKeyboardDidHideNotifica
           // Up/Down
           [UIKeyCommand keyCommandWithInput:UIKeyInputUpArrow modifierFlags:0 action:@selector(didPressArrowKey:)],
           [UIKeyCommand keyCommandWithInput:UIKeyInputDownArrow modifierFlags:0 action:@selector(didPressArrowKey:)],
-          [UIKeyCommand keyCommandWithInput:UIKeyInputLeftArrow modifierFlags:0 action:@selector(didPressArrowKey:)],
-          [UIKeyCommand keyCommandWithInput:UIKeyInputRightArrow modifierFlags:0 action:@selector(didPressArrowKey:)]
           ];
     
     return _keyboardCommands;
