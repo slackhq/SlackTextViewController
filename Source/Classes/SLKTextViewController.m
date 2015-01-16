slackhq/SlackTextViewController

commit c9a5a9b1e94ef35fa2d119d81403db3110a09359

Moves auto-completion text processing to the main thread, hopefully fixing the following odd crashes: https://crashlytics.com/tiny-speck/ios/apps/com.tinyspeck.chatlyio/issues/54ae966365f8dfea158b78b1 https://crashlytics.com/tiny-speck/ios/apps/com.tinyspeck.chatlyio/issues/54ad890e65f8dfea1589cbc6/sessions/54add97a03930001157b346130646663

Source/Classes/SLKTextViewController.m
@@ -1326,31 +1326,24 @@ NSString * const SLKKeyboardDidHideNotification =   @"SLKKeyboardDidHideNotifica
         return [self cancelAutoCompletion];
     }
     
-    // Process in the background
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+    NSRange range;
+    NSString *word = [self.textView slk_wordAtCaretRange:&range];
+    
+    for (NSString *sign in self.registeredPrefixes) {
         
-        NSRange range;
-        NSString *word = [self.textView slk_wordAtCaretRange:&range];
+        NSRange keyRange = [word rangeOfString:sign];
         
-        for (NSString *sign in self.registeredPrefixes) {
+        if (keyRange.location == 0 || (keyRange.length >= 1)) {
             
-            NSRange keyRange = [word rangeOfString:sign];
+            // Captures the detected symbol prefix
+            _foundPrefix = sign;
             
-            if (keyRange.location == 0 || (keyRange.length >= 1)) {
-                
-                // Captures the detected symbol prefix
-                _foundPrefix = sign;
-                
-                // Used later for replacing the detected range with a new string alias returned in -acceptAutoCompletionWithString:
-                _foundPrefixRange = NSMakeRange(range.location, sign.length);
-            }
+            // Used later for replacing the detected range with a new string alias returned in -acceptAutoCompletionWithString:
+            _foundPrefixRange = NSMakeRange(range.location, sign.length);
         }
-        
-        // Forward to the main queue
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self handleProcessedWord:word range:range];
-        });
-    });
+    }
+    
+    [self handleProcessedWord:word range:range];
 }
 
 - (void)handleProcessedWord:(NSString *)word range:(NSRange)range

Source/Classes/SLKTextView.m
@@ -403,9 +403,9 @@ SLKPastableMediaType SLKPastableMediaTypeFromNSString(NSString *string)
 // Safer cursor range (it sometimes exceeds the lenght of the text property).
 - (NSRange)selectedRange
 {
-    NSString *text = self.text;
     NSRange range = [super selectedRange];
-    
+    NSString *text = self.text;
+
     if (range.location > text.length) {
         range.location = text.length;
     }

commit 44180ad5b1e662be445cff4c5a649c165dcd9b45

Fixes issue when becoming first responder, and autocompletion will automatically be triggered: the text input will get hidden. Bug introduced in c9a5a9b.

Source/Classes/SLKTextViewController.m
@@ -1350,7 +1350,7 @@ NSString * const SLKKeyboardDidHideNotification =   @"SLKKeyboardDidHideNotifica
     
     NSString *text = self.textView.text;
     
-    // No need to process for autocompletion if there is no text to process
+    // Skip, when o text to process
     if (text.length == 0) {
         return [self cancelAutoCompletion];
     }
@@ -1358,27 +1358,30 @@ NSString * const SLKKeyboardDidHideNotification =   @"SLKKeyboardDidHideNotifica
     NSRange range;
     NSString *word = [self.textView slk_wordAtCaretRange:&range];
     
-    for (NSString *sign in self.registeredPrefixes) {
+    for (NSString *prefix in self.registeredPrefixes) {
         
-        NSRange keyRange = [word rangeOfString:sign];
+        NSRange keyRange = [word rangeOfString:prefix];
         
         if (keyRange.location == 0 || (keyRange.length >= 1)) {
             
             // Captures the detected symbol prefix
-            _foundPrefix = sign;
+            _foundPrefix = prefix;
             
             // Used later for replacing the detected range with a new string alias returned in -acceptAutoCompletionWithString:
-            _foundPrefixRange = NSMakeRange(range.location, sign.length);
+            _foundPrefixRange = NSMakeRange(range.location, prefix.length);
         }
     }
     
-    [self handleProcessedWord:word range:range];
+    // Forward to the main queue, to be sure it goes into the next run loop
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self handleProcessedWord:word range:range];
+    });
 }
 
 - (void)handleProcessedWord:(NSString *)word range:(NSRange)range
 {
     // Cancel autocompletion if the cursor is placed before the prefix
-    if (self.textView.selectedRange.location <= _foundPrefixRange.location) {
+    if (self.textView.selectedRange.location <= self.foundPrefixRange.location) {
         return [self cancelAutoCompletion];
     }
     