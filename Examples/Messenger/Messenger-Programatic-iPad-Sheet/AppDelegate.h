2df39ad7dbd9b983fcb6c2c39fcc820d680e902b
Bob Spryn <bob@thumbworks.io>
2015-01-23 16:30:58 -0500

    Added a new target with the iPad sheet example, not yet working properly.

---
+++ Examples/Messenger/Messenger-Programatic-iPad-Sheet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Messenger-Programatic-iPad-Sheet
+//
+//  Created by Bob Spryn on 1/23/15.
+//  Copyright (c) 2015 Slack Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- Examples/Messenger/Pods/Local Podspecs/SlackTextViewController.podspec
+++ Examples/Messenger/Pods/Local Podspecs/SlackTextViewController.podspec
@@ -1,4 +1,4 @@
-@version = "1.3.3"
+@version = "1.4.1"
 
 Pod::Spec.new do |s|
   s.name         		= "SlackTextViewController"
