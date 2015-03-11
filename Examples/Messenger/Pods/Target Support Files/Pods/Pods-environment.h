e25b34167fc988bad8227e43222afdbf6e617ca6
dzenbot <iromero@dzen.cl>
2015-03-11 01:53:24 -0300

    Installs Expecta+Snapshots

--- Examples/Messenger/Pods/Target Support Files/Pods/Pods-environment.h
+++ Examples/Messenger/Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,6 +6,24 @@
 // project.
 
 
+// Expecta
+#define COCOAPODS_POD_AVAILABLE_Expecta
+#define COCOAPODS_VERSION_MAJOR_Expecta 0
+#define COCOAPODS_VERSION_MINOR_Expecta 4
+#define COCOAPODS_VERSION_PATCH_Expecta 0
+
+// Expecta+Snapshots
+#define COCOAPODS_POD_AVAILABLE_Expecta_Snapshots
+#define COCOAPODS_VERSION_MAJOR_Expecta_Snapshots 1
+#define COCOAPODS_VERSION_MINOR_Expecta_Snapshots 3
+#define COCOAPODS_VERSION_PATCH_Expecta_Snapshots 2
+
+// FBSnapshotTestCase
+#define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
+#define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
+#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 6
+#define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
+
 // LoremIpsum
 #define COCOAPODS_POD_AVAILABLE_LoremIpsum
 #define COCOAPODS_VERSION_MAJOR_LoremIpsum 1
@@ -15,12 +33,12 @@
 // SlackTextViewController
 #define COCOAPODS_POD_AVAILABLE_SlackTextViewController
 #define COCOAPODS_VERSION_MAJOR_SlackTextViewController 1
-#define COCOAPODS_VERSION_MINOR_SlackTextViewController 4
-#define COCOAPODS_VERSION_PATCH_SlackTextViewController 1
+#define COCOAPODS_VERSION_MINOR_SlackTextViewController 5
+#define COCOAPODS_VERSION_PATCH_SlackTextViewController 0
 
 // SlackTextViewController/Additions
 #define COCOAPODS_POD_AVAILABLE_SlackTextViewController_Additions
 #define COCOAPODS_VERSION_MAJOR_SlackTextViewController_Additions 1
-#define COCOAPODS_VERSION_MINOR_SlackTextViewController_Additions 4
-#define COCOAPODS_VERSION_PATCH_SlackTextViewController_Additions 1
+#define COCOAPODS_VERSION_MINOR_SlackTextViewController_Additions 5
+#define COCOAPODS_VERSION_PATCH_SlackTextViewController_Additions 0
 
