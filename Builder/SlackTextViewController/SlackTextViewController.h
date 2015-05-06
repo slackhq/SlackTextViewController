e7a21f0e12d3ed5c092517e13b1c7da5e92863d4
Hank Bao <hankbao84@gmail.com>
2015-05-06 17:19:31 +0800

    update SlackTextViewController.h to import all public headers and fix framework header problem in project setting

--- Builder/SlackTextViewController/SlackTextViewController.h
+++ Builder/SlackTextViewController/SlackTextViewController.h
@@ -18,3 +18,13 @@
 
 FOUNDATION_EXPORT double SlackTextViewControllerVersionNumber;
 FOUNDATION_EXPORT const unsigned char SlackTextViewControllerVersionString[];
+
+#import <SlackTextViewController/SLKUIConstants.h>
+#import <SlackTextViewController/SLKTextViewController.h>
+#import <SlackTextViewController/SLKTextView.h>
+#import <SlackTextViewController/SLKTextInputbar.h>
+#import <SlackTextViewController/SLKInputAccessoryView.h>
+#import <SlackTextViewController/SLKTypingIndicatorView.h>
+#import <SlackTextViewController/UIView+SLKAdditions.h>
+#import <SlackTextViewController/UIScrollView+SLKAdditions.h>
+#import <SlackTextViewController/SLKTextView+SLKAdditions.h>
