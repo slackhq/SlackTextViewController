bef57a7d0ca980f1d4137f222f3c29ed49e72c44
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2015-09-02 13:51:18 -0300

    Fixes the keyboard panning on iOS 9

--- Source/Classes/SLKInputAccessoryView.m
+++ Source/Classes/SLKInputAccessoryView.m
@@ -24,7 +24,23 @@
 - (void)willMoveToSuperview:(UIView *)newSuperview
 {
     if (newSuperview) {
-        _keyboardViewProxy = newSuperview;
+        if (SLK_IS_IOS9_AND_HIGHER) {
+            
+            NSPredicate *windowPredicate = [NSPredicate predicateWithFormat:@"self isMemberOfClass: %@", NSClassFromString(@"UIRemoteKeyboardWindow")];
+            UIWindow *keyboardWindow = [[[UIApplication sharedApplication].windows filteredArrayUsingPredicate:windowPredicate] firstObject];
+            
+            for (UIView *subview in keyboardWindow.subviews) {
+                for (UIView *hostview in subview.subviews) {
+                    if ([hostview isMemberOfClass:NSClassFromString(@"UIInputSetHostView")]) {
+                        _keyboardViewProxy = hostview;
+                        break;
+                    }
+                }
+            }
+        }
+        else {
+            _keyboardViewProxy = newSuperview;
+        }
     }
 }
 
