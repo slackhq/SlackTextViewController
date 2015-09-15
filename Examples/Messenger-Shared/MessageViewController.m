5dbdd1d3e44e85ee6f51bc52dd9fdafde666e2c1
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2015-09-15 18:25:33 -0300

    Fixes crash in sample project iOS 7 only

--- Examples/Messenger-Shared/MessageViewController.m
+++ Examples/Messenger-Shared/MessageViewController.m
@@ -194,18 +194,23 @@ static NSString *AutoCompletionCellIdentifier = @"AutoCompletionCell";
 - (void)didLongPressCell:(UIGestureRecognizer *)gesture
 {
 #ifdef __IPHONE_8_0
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
-    alertController.modalPresentationStyle = UIModalPresentationPopover;
-    alertController.popoverPresentationController.sourceView = gesture.view.superview;
-    alertController.popoverPresentationController.sourceRect = gesture.view.frame;
-    
-    [alertController addAction:[UIAlertAction actionWithTitle:@"Edit Message" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+    if ([UIAlertController class]) {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+        alertController.modalPresentationStyle = UIModalPresentationPopover;
+        alertController.popoverPresentationController.sourceView = gesture.view.superview;
+        alertController.popoverPresentationController.sourceRect = gesture.view.frame;
+        
+        [alertController addAction:[UIAlertAction actionWithTitle:@"Edit Message" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            [self editCellMessage:gesture];
+        }]];
+        
+        [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:NULL]];
+        
+        [self.navigationController presentViewController:alertController animated:YES completion:nil];
+    }
+    else {
         [self editCellMessage:gesture];
-    }]];
-    
-    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:NULL]];
-    
-    [self.navigationController presentViewController:alertController animated:YES completion:nil];
+    }
 #else
     [self editCellMessage:gesture];
 #endif
