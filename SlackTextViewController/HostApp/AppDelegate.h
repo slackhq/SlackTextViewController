bd7a3d52543ba04b3c6ca3337aaf989b60a015ce
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-01-18 14:50:09 -0800

    Recreates the test targets, fixing previous snapshot tests

---
+++ SlackTextViewController/HostApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HostApp
+//
+//  Created by Ignacio Romero on 1/18/16.
+//  Copyright © 2016 Slack Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlackTextViewController/HostApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HostApp
+//
+//  Created by Ignacio Romero on 1/18/16.
+//  Copyright © 2016 Slack Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
